2d0726093b1b8a51269720cd27ac01c0482d7888
Sandeep Kumar <sandeepsharma7255@gmail.com>
2026-07-28 18:32:13 +0530

    Added Service Worker and PWA manifest for full offline support

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'sandeep-erp-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './dr.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                if(event.request.url.startsWith(self.location.origin)) {
+                    cache.put(event.request, responseToCache);
+                }
+              });
+            return response;
+          }
+        );
+      }).catch(() => {
+          if (event.request.mode === 'navigate') {
+              return caches.match('./index.html');
+          }
+      })
+  );
+});
